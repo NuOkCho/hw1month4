d08a880e2092cf271d983d67b4b8c4fa4ad4c515
Ahmed Muvaza <makbookicloud312@mail.ru>
2023-03-28 17:11:40 +0600

    Initial Commit

---
+++ hw1month4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw1month4
+//
+//  Created by Ahmed Muvaza on 28/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
